1eb6ffb6c609216fcc67c7adc2bc77a3afafdcd5
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2023-09-24 17:18:18 +0200

    Fix bar sprite not properly swapping.

--- HootLib/Components/ScrollBarSpriteSwapper.cs
+++ HootLib/Components/ScrollBarSpriteSwapper.cs
@@ -4,8 +4,8 @@ using UnityEngine.UI;
 namespace HootLib.Components
 {
     /// <summary>
-    /// A component made to be added to the GameObject of a <see cref="ScrollRect"/> which finds any ScrollBar's
-    /// <see cref="Image"/> components and replaces their sprites with the vanilla menu sprites.
+    /// A self-destructing component made to be added to the GameObject of a <see cref="ScrollRect"/> which finds any
+    /// ScrollBar's <see cref="Image"/> components and replaces their sprites with the vanilla menu sprites.
     /// Ideal for making a scrollbar look just like the vanilla ones found in the main menu.
     /// </summary>
     public class ScrollBarSpriteSwapper : MonoBehaviour
@@ -17,16 +17,11 @@ namespace HootLib.Components
         private void Awake()
         {
             scrollRect ??= GetComponent<ScrollRect>();
-            if (scrollBarSprite is null)
-                AddressablesUtility.LoadAsync<Sprite>(AssetFilePaths.ScrollBarSprite)
-                    .Completed += handle => scrollBarSprite = handle.Result;
-            if (scrollHandleSprite is null)
-                AddressablesUtility.LoadAsync<Sprite>(AssetFilePaths.ScrollHandleSprite)
-                    .Completed += handle => scrollHandleSprite = handle.Result;
-        }
-
-        private void Start()
-        {
+            scrollBarSprite ??= AddressablesUtility.LoadAsync<Sprite>(AssetFilePaths.ScrollBarSprite)
+                .WaitForCompletion();
+            scrollHandleSprite ??= AddressablesUtility.LoadAsync<Sprite>(AssetFilePaths.ScrollHandleSprite)
+                .WaitForCompletion();
+            
             if (scrollRect.horizontalScrollbar != null)
                 SwapSprites(scrollRect.horizontalScrollbar);
             if (scrollRect.verticalScrollbar != null)
@@ -38,9 +33,8 @@ namespace HootLib.Components
 
         private void SwapSprites(Scrollbar scrollbar)
         {
-            scrollbar.image.sprite = scrollBarSprite;
-            if (scrollbar.targetGraphic is Image handle)
-                handle.sprite = scrollHandleSprite;
+            scrollbar.GetComponent<Image>().sprite = scrollBarSprite;
+            scrollbar.image.sprite = scrollHandleSprite;
         }
     }
 }
